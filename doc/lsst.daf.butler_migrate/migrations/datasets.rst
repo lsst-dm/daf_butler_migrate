18ca5a2cacd77b2226ca544b178e7c863dd215e2
Andy Salnikov <salnikov@slac.stanford.edu>
2023-04-03 11:09:39 -0700

    Add migration script for datasets manager, schema upgrade to 2.0.0.

--- doc/lsst.daf.butler_migrate/migrations/datasets.rst
+++ doc/lsst.daf.butler_migrate/migrations/datasets.rst
@@ -4,10 +4,14 @@ Migrations for datasets manager
 
 The ``datasets`` tree has two branches:
 
-- ``datasets-ByDimensionsDatasetRecordStorageManager`` which is the older implementation of the manager using integer dataset IDs.
+- ``datasets-ByDimensionsDatasetRecordStorageManager`` which is the older implementation of the manager using integer dataset IDs, this managet class was removed from the code starting with ``w_2023_10``.
 - ``datasets-ByDimensionsDatasetRecordStorageManagerUUID`` which is the new implementation using UUIDs.
 
-The regular revision tree has migrations defined for all manager versions that have existed in our code, but none of these migrations were actually implemented.
+The regular revision tree has migrations defined for all manager versions that have existed in our code, but only few of these migrations were actually implemented.
+
+
+One-shot migration to UUID
+==========================
 
 There is a one-shot migration implemented for migration from ``ByDimensionsDatasetRecordStorageManager`` 1.0.0 to ``ByDimensionsDatasetRecordStorageManagerUUID`` 1.0.0::
 
@@ -17,3 +21,11 @@ There is a one-shot migration implemented for migration from ``ByDimensionsDatas
     <base> -> 059cc7b7ef13 (datasets), This is an initial pseudo-revision of the 'datasets/int_1.0.0_to_uuid_1.0.0' tree.
 
 The migration script for this one-shot migration is in `2101fbf51ad3.py <https://github.com/lsst-dm/daf_butler_migrate/blob/main/migrations/_oneshot/datasets/int_1.0.0_to_uuid_1.0.0/2101fbf51ad3.py>`_.
+
+
+ByDimensionsDatasetRecordStorageManagerUUID 1.0.0 to 2.0.0
+==========================================================
+
+Migration script: `4e2d7a28475b.py <https://github.com/lsst-dm/daf_butler_migrate/blob/main/migrations/datasets/4e2d7a28475b.py>`_
+
+Changes the type of ``ingest_date`` column in ``dataset`` table from ``TIMESTAMP`` to ``BIGINT``, containing nanoseconds since epoch like many other timestamp columns.
---
+++ migrations/datasets/4e2d7a28475b.py
@@ -0,0 +1,193 @@
+"""Migration script for ByDimensionsDatasetRecordStorageManagerUUID 2.0.0.
+
+Revision ID: 4e2d7a28475b
+Revises: 2101fbf51ad3
+Create Date: 2023-03-31 22:25:32.651155
+
+"""
+
+import datetime
+import itertools
+import logging
+from collections.abc import Callable
+from typing import Any
+
+import sqlalchemy as sa
+from alembic import context, op
+from lsst.daf.butler_migrate.butler_attributes import ButlerAttributes
+
+# revision identifiers, used by Alembic.
+revision = "4e2d7a28475b"
+down_revision = "2101fbf51ad3"
+branch_labels = None
+depends_on = None
+
+_LOG = logging.getLogger(f"lsst.daf.butler_migrate.{__name__}")
+
+# Manager name
+MANAGER = (
+    "lsst.daf.butler.registry.datasets.byDimensions._manager.ByDimensionsDatasetRecordStorageManagerUUID"
+)
+
+_EPOCH = datetime.datetime(1970, 1, 1)
+
+
+def _convert_to_ns(dt: datetime.datetime) -> int:
+    """Convert datetime to nanoseconds."""
+    microseconds = (dt - _EPOCH) // datetime.timedelta(microseconds=1)
+    return microseconds * 1000
+
+
+def _convert_to_datetime(nsec: int) -> datetime.datetime:
+    """Convert nanoseconds to datetime."""
+    microseconds = nsec // 1000
+    return datetime.datetime.utcfromtimestamp(microseconds / 1e6)
+
+
+def upgrade() -> None:
+    """Upgrade from version 1.0.0 to version 2.0.0.
+
+    Summary of changes:
+
+        - ingest_date column type changed from database-native TIMESTAMP
+          to nanoseconds-since-epoch in TAI scale.
+    """
+    bind = op.get_bind()
+    if bind.dialect.name == "postgresql":
+        # Keep it at miscrosecond resolution
+        _migrate_pg(
+            sa.BIGINT, None, "CAST(EXTRACT(EPOCH FROM ingest_date) * 1000000 AS BIGINT) * 1000", "2.0.0"
+        )
+    else:
+        _migrate_default(sa.BIGINT, _convert_to_ns, "2.0.0")
+
+
+def downgrade() -> None:
+    """Downgrade from version 2.0.0 to version 1.0.0."""
+    bind = op.get_bind()
+    if bind.dialect.name == "postgresql":
+        # Convert from nanoseconds to timestamp. TO_TIMESTAMP returns timestamp
+        # with timezone, and `_migrate_pg` does SET TIME ZONE 0 to keep things
+        # consistent.
+        _migrate_pg(sa.TIMESTAMP, sa.sql.func.now(), "TO_TIMESTAMP(ingest_date / 1000000000.)", "1.0.0")
+    else:
+        _migrate_default(sa.TIMESTAMP, _convert_to_datetime, "1.0.0")
+
+
+def _migrate_default(
+    column_type: type,
+    convert_method: Callable[[datetime.datetime], int] | Callable[[int], datetime.datetime],
+    version: str,
+) -> None:
+    """Default migration method that does not rely on optimizations.
+
+    This method creates a temporary table which is filled with the converted
+    timestamps, and then updates dataset table from that temporary table. This
+    was only tested with SQLite, may not work with other backends.
+    """
+    mig_context = context.get_context()
+    bind = op.get_bind()
+    assert bind is not None, "Migration only works on actual connection."
+    schema = mig_context.version_table_schema
+    metadata = sa.schema.MetaData(schema=schema)
+
+    table = sa.schema.Table("dataset", metadata, autoload_with=bind, schema=schema)
+
+    # Create a temporary table.
+    tmp_table = sa.schema.Table(
+        "dataset_migration_tmp",
+        metadata,
+        sa.schema.Column("id", table.columns["id"].type, primary_key=True),
+        sa.schema.Column("ingest_date", column_type, nullable=False),
+        prefixes=["TEMPORARY"],
+        schema=sa.schema.BLANK_SCHEMA,
+    )
+    tmp_table.create(bind)
+
+    # There may be very many records in dataset table to fit everything in
+    # memory, so split the whole thing on dataset_type_id.
+    query = sa.select(table.columns["dataset_type_id"]).select_from(table).distinct()
+    result = bind.execute(query).scalars()
+    dataset_type_ids = sorted(result)
+    _LOG.info("Found %s dataset types in dataset table", len(dataset_type_ids))
+
+    for dataset_type_id in dataset_type_ids:
+        # Extract ingest date for datasets.
+        query = (
+            sa.select(table.columns["id"], table.columns["ingest_date"])
+            .select_from(table)
+            .where(table.columns["dataset_type_id"] == dataset_type_id)
+        )
+        result = bind.execute(query)
+        rows = [(dataset_id, convert_method(ingest_date)) for dataset_id, ingest_date in result]
+        _LOG.info("Found %s rows for in dataset type %s", len(rows), dataset_type_id)
+
+        iterator = iter(rows)
+        count = 0
+        while chunk := list(itertools.islice(iterator, 1000)):
+            query = tmp_table.insert().values(chunk)
+            result = bind.execute(query)
+            count += result.rowcount
+        _LOG.info("Inserted %s rows into temporary table", count)
+
+    with op.batch_alter_table("dataset", schema) as batch_op:
+        # Change the type of the column.
+        batch_op.alter_column(  # type: ignore[attr-defined]
+            "ingest_date", type_=column_type, server_default=None
+        )
+
+    # Update ingest date from a temporary table.
+    query = table.update().values(
+        ingest_date=sa.select(tmp_table.columns["ingest_date"])
+        .where(tmp_table.columns["id"] == table.columns["id"])
+        .scalar_subquery()
+    )
+    result = bind.execute(query)
+    _LOG.info("Updated %s rows in dataset table", result.rowcount)
+
+    # Update manager schema version.
+    attributes = ButlerAttributes(bind, schema)
+    attributes.update_manager_version(MANAGER, version)
+
+
+def _migrate_pg(
+    column_type: type,
+    server_default: Any,
+    using: str,
+    version: str,
+) -> None:
+    """Migration method with PostgreSQL-specific optimizations.
+
+    Notes
+    -----
+    Postgres allows in-place type update with re-calculation of the new data
+    values with its special USING syntax which provides an expression for
+    calculating new data values.
+    """
+    mig_context = context.get_context()
+    bind = op.get_bind()
+    assert bind is not None, "Migration only works on actual connection."
+    schema = mig_context.version_table_schema
+
+    # Some conversions may involve conversion of timestamps, make sure
+    # everything is done in UTC.
+    bind.execute(sa.text("SET TIME ZONE 0"))
+
+    _LOG.info("Start updating dataset table")
+    # Change the type of the column.
+    if server_default is None:
+        # If removing a default it has to be done first
+        op.alter_column("dataset", "ingest_date", schema=schema, server_default=None)
+    op.alter_column(
+        "dataset",
+        "ingest_date",
+        schema=schema,
+        type_=column_type,
+        server_default=server_default,
+        postgresql_using=using,
+    )
+    _LOG.info("Finished updating dataset table")
+
+    # Update manager schema version.
+    attributes = ButlerAttributes(bind, schema)
+    attributes.update_manager_version(MANAGER, version)
